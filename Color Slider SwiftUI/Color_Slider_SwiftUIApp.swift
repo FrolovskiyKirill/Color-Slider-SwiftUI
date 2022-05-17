0767d86467e9ecd0780553f5f8bd47dc833b4e08
Kirill <frolkv@gmail.com>
2022-05-17 12:10:08 +0300

    Initial Commit

---
+++ Color Slider SwiftUI/Color_Slider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Slider_SwiftUIApp.swift
+//  Color Slider SwiftUI
+//
+//  Created by Кирилл on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Color_Slider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
